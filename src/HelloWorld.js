2dae3158dbdd5c9a1fa46acadf5dc63729c4228a
Tienbot <tien.ru@mail.ru>
2024-10-05 02:46:58 +0300

    first commit

---
+++ src/HelloWorld.js
@@ -0,0 +1,13 @@
+
+import React, {Component} from "react";
+import "./HelloWorld.css";
+
+class HelloWorld extends Component {
+  render() {
+    return (
+      <div className="helloContainer">
+        <h1>Привет, Мир!</h1>
+      </div>
+); }
+}
+export default HelloWorld;
